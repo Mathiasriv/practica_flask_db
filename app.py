b894395636f7dd8481ab84e34e5af81d449a428f
Matias Rivadeneira <matiasriv66@hotmail.com>
2023-10-21 14:13:24 -0300

    agregado users

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from person import Person
+import jwt
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'user_flask'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'app_flask_db'
 app.config['MYSQL_PASSWORD'] ='1234'
-app.config['MYSQL_DB'] = 'db_flask_2'
+app.config['MYSQL_DB'] = 'db_flask_1'
+
+app.config['SECRET_KEY'] = 'key1234'
 
 
 mysql = MySQL(app)
+
+@app.route('/login', methods = ['POST'])
+def login():
+    return jsonify({'index'})
+
+
 
 @app.route('/persons', methods = ['GET'])
 def get_all_persons():
@@ -33,6 +42,14 @@ def create_persons():
     email = body['email']
 
     cur = mysql.connect.cursor()
+
+
+    cur.execute('SELECT * FROM person WHERE email= %s', (email,) )
+    row = cur.fetchone()
+    if row:
+        return jsonify({'message': "Ya existe el email"})
+
+
     cur.execute('INSERT INTO person (name, surname, dni, email) VALUES (%s, %s, %s, %s)', (name, surname, dni, email))
     mysql.connection.commit()    
 
@@ -62,11 +79,15 @@ def update(id):
     surname = body['surname']
     dni = body['dni']
     email = body['email']
-    
+    cur = mysql.connection.cursor()
+    cur.execute('UPDATE person SET name = %s, surname = %s, dni = %s,  email = %s, where id = %s' (name, surname, dni, email, id))
+    mysql.connection.commit()
     return jsonify({'id':id,'name':name, 'surname': surname, 'dni': dni, 'email': email })
 
 @app.route('/persons/<int:id>', methods = ['DELETE'])
 def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE ')
     return jsonify({'message': 'delete'})
 
 
--- db.sql
+++ db.sql
@@ -14,9 +14,15 @@ INSERT INTO person VALUES
 (2,'ana', 'Perez', 5678, 'ana@gmail.com');
 
 
+CREATE TABLE IN NOT EXISTS users(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    PRIMARY KEY (id)
+)
 
-
-
+INSERT INTO users VALUES
+(1, 'carlos', 'pass');
     
 
 
