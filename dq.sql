28ff8937a2350512a6a57dfbefdda43457d67c0d
Matias Rivadeneira <matiasriv66@hotmail.com>
2023-10-30 20:52:40 -0300

    terminado backend

---
+++ dq.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE IF NOT EXISTS  db_flask_1;
+USE db_flask_1;
+CREATE TABLE IF NOT EXISTS person(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    dni INT(8) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) 
+INSERT INTO person VALUES
+(1, 'Juan', 'Álvarez' , 12345678, 'juan@mail.com'),
+(2, 'Ana', 'Perez' , 87654321, 'ana@mail.com');
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) 
+
+INSERT INTO users VALUES
+(1, 'carlos', 'pass'),
+(2, 'ana', 'pass');
+
+CREATE TABLE IF NOT EXISTS client(
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    id_user INT(10),
+    FOREIGN KEY(id_user) REFERENCES users(id) 
+) 
+
+INSERT INTO client VALUES
+(1, 'juan', 1),
+(2, 'maria', 1),
+(3, 'marta', 2),
+(4, 'jose', 2);
